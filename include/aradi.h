6676446aa49c3a23349c10f5f1cea3aaeaf02178
eduardorbl <j260551@dac.unicamp.br>
2025-05-12 10:37:59 -0300

    Updating aradi.h

--- include/aradi.h
+++ include/aradi.h
@@ -11,4 +11,9 @@ void aradi_encrypt(const uint32_t plaintext[4], const uint32_t round_keys[17][4]
 // Decrypts a 128-bit block using ARADI (4 x 32-bit words)
 void aradi_decrypt(const uint32_t ciphertext[4], const uint32_t round_keys[17][4], uint32_t output[4]);
 
+// Encrypts a 128-bit block using ARADI with bit shuffling
+void aradi_encrypt_shuffle(const uint32_t plaintext[4], const uint32_t round_keys[17][4], uint32_t output[4]);
+
+// Decrypts a 128-bit block using ARADI with bit shuffling
+void aradi_decrypt_shuffle(const uint32_t ciphertext[4], const uint32_t round_keys[17][4], uint32_t output[4]);
 #endif // ARADI_H
